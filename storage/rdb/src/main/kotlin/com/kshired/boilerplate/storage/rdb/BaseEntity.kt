7a95ffcd029492c433b906d755fed42e3f20b841
kshired <shiroed1211@gmail.com>
2023-01-30 23:04:01 +0900

    fix : base entity 수정

--- storage/rdb/src/main/kotlin/com/kshired/boilerplate/storage/rdb/BaseEntity.kt
+++ storage/rdb/src/main/kotlin/com/kshired/boilerplate/storage/rdb/BaseEntity.kt
@@ -1,23 +1,15 @@
 package com.kshired.boilerplate.storage.rdb
 
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.ZonedDateTime
 
 @MappedSuperclass
-internal abstract class BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0
-
+internal abstract class BaseEntity(
     @CreationTimestamp
-    val createdAt: ZonedDateTime? = null
+    val createdAt: ZonedDateTime? = null,
 
     @UpdateTimestamp
-    var updatedAt: ZonedDateTime? = null
-        protected set
-}
+    var updatedAt: ZonedDateTime? = null,
+)
--- storage/rdb/src/main/kotlin/com/kshired/boilerplate/storage/rdb/user/UserEntity.kt
+++ storage/rdb/src/main/kotlin/com/kshired/boilerplate/storage/rdb/user/UserEntity.kt
@@ -4,11 +4,18 @@ import com.kshired.boilerplate.domain.user.User
 import com.kshired.boilerplate.storage.rdb.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "users")
 internal class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
     @Column(name = "username")
     val username: String,
 
@@ -18,6 +25,7 @@ internal class UserEntity(
     companion object {
         fun fromDomain(domain: User) : UserEntity {
             return UserEntity(
+                id = domain.id,
                 username = domain.username,
                 email = domain.email
             )
